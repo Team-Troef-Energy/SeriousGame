b42892b3badddc529d6d467e872cd3d6e4b3392f
abdullah03366 <abdullah.sanli@student.hu.nl>
2024-10-11 13:30:08 +0200

    Domein is geimplementeerd volgens klassendiagram op lucidchart

---
+++ back-end/src/main/java/nl/hu/serious_game/domain/Accu.java
@@ -0,0 +1,8 @@
+package nl.hu.serious_game.domain;
+
+public class Accu {
+    private float output;
+    private float currentCharge;
+    private float capacity;
+    private int coinValue;
+}
---
+++ back-end/src/main/java/nl/hu/serious_game/domain/Transformator.java
@@ -0,0 +1,11 @@
+package nl.hu.serious_game.domain;
+
+import java.util.ArrayList;
+
+public class Transformator {
+    private int currentBalance;
+    private int capacity;
+    private ArrayList<House> houses;
+    private ArrayList<Windmill> windmills;
+    private ArrayList<Accu> accus;
+}
