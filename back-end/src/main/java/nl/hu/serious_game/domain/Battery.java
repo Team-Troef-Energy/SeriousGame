785ec2acb1562c73257096702e229486397a6c23
kaischreurs <70720881+kaischreurs@users.noreply.github.com>
2024-10-23 11:44:41 +0200

    accu logica is als het goed is af

--- back-end/src/main/java/nl/hu/serious_game/domain/Battery.java
+++ back-end/src/main/java/nl/hu/serious_game/domain/Battery.java
@@ -1,8 +1,37 @@
 package nl.hu.serious_game.domain;
 
 public class Battery {
-    private float output;
+    private float chargeSpeed;
+    private float dischargeSpeed;
     private float currentCharge;
     private float capacity;
     private int coinValue;
+
+    Battery(int amount) {
+        capacity = 13.5f * amount;
+        chargeSpeed = 5 * amount;
+        dischargeSpeed = 11.5f * amount;
+    }
+
+    // Takes the electricity that needs to be handled.
+    // Returns the electricity that remains afterwards.
+    public Electricity use(Electricity electricity) {
+        if (electricity.amount() == 0) {
+            return electricity;
+        }
+        if (electricity.direction() == Direction.DEMAND) {
+            float available = Math.min(this.dischargeSpeed, this.currentCharge);
+            float flow = Math.min(available, electricity.amount());
+            this.currentCharge -= flow;
+            return new Electricity(electricity.amount() - flow, Direction.DEMAND);
+        }
+        if (electricity.direction() == Direction.PRODUCTION) {
+            float available = Math.min(this.chargeSpeed, this.capacity - this.currentCharge);
+            float flow = Math.min(available, electricity.amount());
+            this.currentCharge += flow;
+            return new Electricity(electricity.amount() - flow, Direction.PRODUCTION);
+        }
+        throw new RuntimeException("Unreachable");
+     }
+
 }
