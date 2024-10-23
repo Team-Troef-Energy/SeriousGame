489e369def6ffdefda2f93f1a27bf1de90e76aad
l_you <youri.levoye@student.hu.nl>
2024-10-23 13:21:53 +0200

    Factor waardes toegevoegd aan seizoenen met specifieke getter methodes om deze op te halen waar nodig

--- back-end/src/main/java/nl/hu/serious_game/domain/Season.java
+++ back-end/src/main/java/nl/hu/serious_game/domain/Season.java
@@ -1,8 +1,24 @@
 package nl.hu.serious_game.domain;
 
 public enum Season {
-    SPRING,
-    SUMMER,
-    AUTUMN,
-    WINTER
+    SPRING(0.89, 1.25),
+    SUMMER(1, 1),
+    AUTUMN(0.54, 1.25),
+    WINTER(0.27, 1.5);
+
+    private final double solarPanelFactor;
+    private final double houseBaseConsumptionFactor;
+
+    Season(double solarPanelFactor, double houseBaseConsumptionFactor) {
+        this.solarPanelFactor = solarPanelFactor;
+        this.houseBaseConsumptionFactor = houseBaseConsumptionFactor;
+    }
+
+    public double getSolarPanelFactor() {
+        return solarPanelFactor;
+    }
+
+    public double getHouseBaseConsumptionFactor() {
+        return houseBaseConsumptionFactor;
+    }
 }
