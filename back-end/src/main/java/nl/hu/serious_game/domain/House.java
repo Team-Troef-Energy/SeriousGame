b458da9b84b46e31754271ce19adbd4daf004b60
l_you <youri.levoye@student.hu.nl>
2024-10-25 13:18:47 +0200

    Methodes aangemaakt om de levering van zonnepanelen te bepalen op een huis

--- back-end/src/main/java/nl/hu/serious_game/domain/House.java
+++ back-end/src/main/java/nl/hu/serious_game/domain/House.java
@@ -7,7 +7,25 @@ public class House {
     private float maxCurrent;
     private boolean hasHeatpump;
     private boolean hasElectricVehicle;
-    private float totalSolarpanels;
+    private int totalSolarPanels;
     private ArrayList<Battery> batteries;
     private DayProfile dayProfile;
+
+    public House (int id, int totalSolarPanels, DayProfile dayProfile) {
+        this.id = id;
+        this.totalSolarPanels = totalSolarPanels;
+        this.dayProfile = dayProfile;
+    }
+
+    public DayProfile getDayProfile() {
+        return dayProfile;
+    }
+
+    public float getSolarPanelOutput(int hour) {
+        if (hour < 0 || hour > 23) {
+            throw new IllegalArgumentException("Hour must be between 0 and 23");
+        }
+
+        return totalSolarPanels * dayProfile.getValue(hour, "SolarPanelProduction");
+    }
 }
