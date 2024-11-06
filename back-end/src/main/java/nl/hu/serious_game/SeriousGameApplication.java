8947fccaa886d9a727322fab3e01488071689f85
l_you <youri.levoye@student.hu.nl>
2024-11-06 11:40:25 +0100

    DataSourceAutoConfig excluded zodat de applicatie start zonder datasource configuratie (aangezien wij nu geen db hebben)

--- back-end/src/main/java/nl/hu/serious_game/SeriousGameApplication.java
+++ back-end/src/main/java/nl/hu/serious_game/SeriousGameApplication.java
@@ -2,8 +2,11 @@ package nl.hu.serious_game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =
+		{DataSourceAutoConfiguration.class}
+)
 public class SeriousGameApplication {
 
 	public static void main(String[] args) {
