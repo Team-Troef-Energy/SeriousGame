f1558a996d6df8b845338476a885a0eb8cf06cad
l_you <youri.levoye@student.hu.nl>
2024-11-06 13:26:00 +0100

    Clone implementatie toegevoegd en uitgewerkt per klasse die dit nodig heeft om zo het Prototype pattern toe te passen

--- back-end/src/main/java/nl/hu/serious_game/domain/Level.java
+++ back-end/src/main/java/nl/hu/serious_game/domain/Level.java
@@ -3,7 +3,7 @@ package nl.hu.serious_game.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Level {
+public class Level implements Cloneable {
     private Season season;
     private int startTime;
     private int endTime;
@@ -40,5 +40,18 @@ public class Level {
 
     public int getEndTime() {
         return endTime;
+    }
+
+    public Level clone() {
+        try {
+            Level clone = (Level) super.clone();
+            clone.transformers = new ArrayList<>();
+            for (Transformer transformer : this.transformers) {
+                clone.transformers.add(transformer.clone());
+            }
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Cloning failed");
+        }
     }
 }
