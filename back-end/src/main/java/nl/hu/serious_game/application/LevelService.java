a93629a75ce3f8be1fc106089f0f9db256ea2d52
l_you <youri.levoye@student.hu.nl>
2024-11-06 14:02:38 +0100

    Start methode aangemaakt voor service om aan de gebruiker de initiele staat te laten zien.

--- back-end/src/main/java/nl/hu/serious_game/application/LevelService.java
+++ back-end/src/main/java/nl/hu/serious_game/application/LevelService.java
@@ -1,7 +1,27 @@
 package nl.hu.serious_game.application;
 
+import nl.hu.serious_game.Runner;
+import nl.hu.serious_game.domain.Level;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LevelService {
+
+    private final Runner runner;
+
+    @Autowired
+    public LevelService(Runner runner) {
+        this.runner = runner;
+    }
+
+    // Returns the level that has been selected to show to the user so they know what to start with.
+    public Level startLevel(int levelNumber) {
+        switch (levelNumber) {
+            case 1:
+                return runner.getLevel1();
+            default:
+                throw new IllegalArgumentException("Invalid level number");
+        }
+    }
 }
