209e7ddebdae3c8666f0c1250c07ad5cca0cc993
l_you <youri.levoye@student.hu.nl>
2024-12-04 12:29:33 +0100

    Level pagina toegevoegd aan router

--- front-end/src/router/Router.ts
+++ front-end/src/router/Router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TestPage from "../pages/TestPage.vue";
 import Playground from "../pages/Playground.vue";
+import Level from "../pages/Level.vue";
 
 const routes = [
     { path: '/', name: 'Home', component: TestPage },
     { path: '/playground', name: 'Playground', component: Playground },
+    { path: '/level', name: 'Level', component: Level },
 ];
 
 const router = createRouter({
