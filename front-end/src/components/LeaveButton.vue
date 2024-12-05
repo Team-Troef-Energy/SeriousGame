674a2a1d630e5e341f495cb6d0c42100823871bb
abdullah03366 <abdullah.sanli@student.hu.nl>
2024-12-05 13:25:33 +0100

    Basic Leavebutton toegevoegd

---
+++ front-end/src/components/LeaveButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn
+    color="red"
+    @click="handleClick"
+    style="padding: 7px 25px; border-radius: 20px">
+    Level verlaten
+  </v-btn>
+</template>
+
+<script>
+  export default {
+    name: 'LeaveButton',
+    methods: {
+      handleClick() {
+        console.log("level left"); // Functionaliteit toevoegen
+      },
+    },
+  }
+</script>
