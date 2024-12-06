463a2ab6ef683324c23fba07075328dec3eacc48
l_you <youri.levoye@student.hu.nl>
2024-12-06 13:01:18 +0100

    Level pagina geupdate met het gebruik van Pixi.js canvas, positioneringslogica en implementatie van connectie-lijn logica

--- front-end/src/pages/Level.vue
+++ front-end/src/pages/Level.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="level-container">
-    <div class="sky"></div>
-    <div class="ground">
-      <div class="grid">
-        <div v-for="n in 100" :key="n" class="grid-item">
-          <Transformer v-if="isTransformerPosition(n)" />
-          <House v-if="isHousePosition(n)" />
-        </div>
-      </div>
+    <div ref="gameCanvas" class="game-canvas">
+      <template v-for="transformer in transformers">
+        <ConnectionLine
+            v-for="house in transformer.houses"
+            :key="'connection-' + house.id"
+            :x1="(transformerPositions[transformer.id - 1] % 10) * 150 + 100"
+            :y1="Math.floor(transformerPositions[transformer.id - 1] / 10) * 80 + 125"
+            :x2="(housePositions[house.id - 1] % 10) * 150 + 100"
+            :y2="Math.floor(housePositions[house.id - 1] / 10) * 80 + 130"
+        />
+      </template>
+      <Transformer
+          v-for="(pos, index) in transformerPositions"
+          :key="'transformer-' + index"
+          :style="{ position: 'absolute', left: (pos % 10) * 150 + 'px', top: Math.floor(pos / 10) * 80 + 'px' }"
+      />
+      <template v-for="transformer in transformers">
+        <House
+            v-for="house in transformer.houses"
+            :key="'house-' + house.id"
+            :style="{ position: 'absolute', left: (housePositions[house.id - 1] % 10) * 150 + 'px', top: Math.floor(housePositions[house.id - 1] / 10) * 80 + 'px' }"
+        />
+      </template>
     </div>
   </div>
 </template>
@@ -15,46 +30,44 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
 import Transformer from '../components/Transformer.vue';
-import House from '../components/Transformer.vue'; //TODO: Change to House component when created
+import House from '../components/House.vue';
+import ConnectionLine from '../components/ConnectionLine.vue';
 
 export default defineComponent({
   name: 'Level',
   components: {
     Transformer,
     House,
+    ConnectionLine,
   },
   setup() {
+    const gameCanvas = ref<HTMLDivElement | null>(null);
     const transformerPositions = ref<number[]>([]);
     const housePositions = ref<number[]>([]);
+    const transformers = ref<{ id: number, houses: { id: number }[] }[]>([]);
+
+    const generatePositions = (count: number, start: number): number[] => {
+      const positions = [];
+      for (let i = 0; i < count; i++) {
+        positions.push(start + i);
+      }
+      return positions;
+    };
 
     onMounted(async () => {
       const response = await fetch('/LevelDummyData.json');
       const data = await response.json();
 
-      if (data.transformerCount === 1) {
-        transformerPositions.value = [23];
-      } else if (data.transformerCount === 2) {
-        transformerPositions.value = [22, 24];
-      }
-
-      if (data.houseCount === 1) {
-        housePositions.value = [39];
-      } else if (data.houseCount === 2) {
-        housePositions.value = [39, 43];
-      }
+      transformerPositions.value = generatePositions(data.transformers.length, 20);
+      housePositions.value = generatePositions(data.transformers.reduce((acc, transformer) => acc + transformer.houses.length, 0), 50);
+      transformers.value = data.transformers;
     });
 
-    const isTransformerPosition = (index: number) => {
-      return transformerPositions.value.includes(index);
-    };
-
-    const isHousePosition = (index: number) => {
-      return housePositions.value.includes(index);
-    };
-
     return {
-      isTransformerPosition,
-      isHousePosition,
+      gameCanvas,
+      transformerPositions,
+      housePositions,
+      transformers,
     };
   },
 });
@@ -63,32 +76,17 @@ export default defineComponent({
 <style scoped>
 .level-container {
   height: 100vh;
+  width: 50vw;
   display: flex;
   flex-direction: column;
+  position: relative;
 }
 
-.sky {
-  background-color: #99e5ff;
-  height: 20%;
-}
-
-.ground {
-  background-color: #309a30;
-  height: 80%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.grid {
-  display: grid;
-  grid-template-columns: repeat(9, 1fr);
-  grid-template-rows: repeat(9, 1fr);
+.game-canvas {
   width: 100%;
   height: 100%;
-}
-
-.grid-item {
-  border: 1px solid rgba(0, 0, 0, 0.1);
+  position: relative;
+  background: url('./grass.jpg') no-repeat center center;
+  background-size: cover;
 }
 </style>
