29bd6f34fad0ffd26c1bf348efe21f709eeb1506
abdullah03366 <abdullah.sanli@student.hu.nl>
2024-12-06 12:32:10 +0100

    T-242 vuetify toegevoegd

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -5,6 +5,12 @@ import TestPage from "./pages/TestPage.vue";
 import "./style.css";
 import App from "./App.vue";
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const routes = [
   { path: "/", component: App },
   { path: "/test", component: TestPage },
@@ -15,4 +21,9 @@ const router = createRouter({
   routes,
 });
 
-createApp(App).use(router).mount("#app");
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
