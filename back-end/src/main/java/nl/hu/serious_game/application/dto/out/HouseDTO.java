df6ed2f758754137a5317d5dbda0f3c50b0e8673
l_you <youri.levoye@student.hu.nl>
2024-12-13 12:09:34 +0100

    DTO uitgebreid met constructor en extra attributen voor congestie en maximale current om weer te geven in de front-end

--- back-end/src/main/java/nl/hu/serious_game/application/dto/out/HouseDTO.java
+++ back-end/src/main/java/nl/hu/serious_game/application/dto/out/HouseDTO.java
@@ -1,11 +1,18 @@
 package nl.hu.serious_game.application.dto.out;
 
+import nl.hu.serious_game.domain.HouseOptions;
+
 public record HouseDTO(
         int id,
         CurrentDTO current,
         BatteryDTO batteries,
         int solarpanels,
         float production,
-        float consumption
-) {
+        float consumption,
+        boolean hasCongestion,
+        float maxCurrent) {
+
+    public HouseDTO(int id, CurrentDTO current, BatteryDTO batteries, int solarpanels, float production, float consumption, HouseOptions houseOptions) {
+        this(id, current, batteries, solarpanels, production, consumption, houseOptions.hasCongestion(), houseOptions.maxCurrent());
+    }
 }
