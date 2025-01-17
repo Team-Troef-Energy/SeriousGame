8f7599910aa146d8cd0ba700c43ef0ccb0e3b8e0
kaischreurs <70720881+kaischreurs@users.noreply.github.com>
2025-01-17 13:54:20 +0100

    zou nu ook op server moeten werken

--- front-end/src/utils/api.ts
+++ front-end/src/utils/api.ts
@@ -1,54 +1,6 @@
 export async function fetchStartLevel(levelNumber: string): Promise<any> {
   try {
-    const response = await fetch(
-        `http://localhost:8080/levels/start/${levelNumber}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-    );
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
-    throw error;
-  }
-}
-
-export async function fetchUpdateLevel(
-  levelNumber: string,
-  levelUpdateDto: any
-): Promise<any> {
-  try {
-    const response = await fetch(
-      "http://localhost:8080/levels/update/" + levelNumber,
-      {
-        method: "POST",
-        body: JSON.stringify(levelUpdateDto),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      }
-    );
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
-    throw error;
-  }
-}
-
-export async function fetchCountLevels(): Promise<any> {
-  try {
-    const response = await fetch("http://localhost:8080/levels/count", {
+    const response = await fetch(`http://${getHost()}/levels/start/${levelNumber}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -63,4 +15,49 @@ export async function fetchCountLevels(): Promise<any> {
     console.error("There was a problem with the fetch operation:", error);
     throw error;
   }
+}
+
+export async function fetchUpdateLevel(levelNumber: string, levelUpdateDto: any): Promise<any> {
+  try {
+    const response = await fetch(`http://${getHost()}/levels/update/` + levelNumber, {
+      method: "POST",
+      body: JSON.stringify(levelUpdateDto),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+    throw error;
+  }
+}
+
+export async function fetchCountLevels(): Promise<any> {
+  try {
+    console.log(location.hostname);
+    const response = await fetch(`http://${getHost()}/levels/count`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+    throw error;
+  }
+}
+
+function getHost() {
+  if (location.hostname === "localhost") return "localhost:8080";
+  return "troeftest.duckdns.org:5001";
 }
