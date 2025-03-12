b519cb66629941bcaa45e8db7d0739dc72aa99db
wessel <wesselvandalen@gmail.com>
2025-03-12 15:57:13 +0100

    Added register route + page

---
+++ front-end/src/pages/RegisterPage.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'RegisterPage'
+});
+</script>
+
+<template>
+    <div class="register-page-container">
+        <p>Register page</p>
+    </div>
+</template>
+
+<style scoped>
+.register-page-container {
+    
+}
+</style>
