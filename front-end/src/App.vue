6a8683826625c79c95a8f5d4fa465858c5181f99
JonathanvdGHU <jonathan.vandegiessen@student.hu.nl>
2025-03-12 12:16:01 +0100

    created header component

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,58 +1,12 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="container">
-    <header>
-      <div class="header-grid">
-        <img src="/troef-logo.png" alt="logo" />
-        <h1>TROEF Energy - Serious game</h1>
-      </div>
-    </header>
+  <div>
+    <Header/>
     <main>
       <RouterView />
     </main>
   </div>
 </template>
 
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  width: 100vw;
-}
-
-header {
-  text-align: center;
-  padding: 1em;
-  background: linear-gradient(to right, #eeffff 80%, #e3d26b);
-  font-size: 0.4rem;
-  color: #000000;
-}
-
-main {
-  flex: 1;
-  overflow: auto;
-  overflow-y: hidden !important;
-}
-
-.header-grid {
-  display: grid;
-  grid-template-columns: 50px 1fr 50px;
-  align-items: center;
-}
-
-header img {
-  height: 50px;
-  grid-column: 1;
-}
-
-header h1 {
-  margin: 0;
-  font-size: 3em;
-  color: #3b3b3b;
-  font-weight: 600;
-  grid-column: 2;
-  text-align: center;
-}
-</style>
+<script setup lang="ts">
+import Header from "./components/global/Header.vue";
+</script>
---
+++ front-end/src/components/global/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <header>
+        <div class="header-grid">
+            <img src="/troef-logo.png" alt="logo" />
+            <h1>TROEF Energy - Serious game</h1>
+        </div>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: 'Header',
+});
+</script>
+
+<style>
+header {
+    text-align: center;
+    padding: 1em;
+    background: linear-gradient(to right, #eeffff 80%, #e3d26b);
+    font-size: 0.4rem;
+    color: #000000;
+}
+
+.header-grid {
+    display: grid;
+    grid-template-columns: 50px 1fr 50px;
+    align-items: center;
+}
+
+header img {
+    height: 50px;
+    grid-column: 1;
+}
+
+header h1 {
+    margin: 0;
+    font-size: 3em;
+    color: #3b3b3b;
+    font-weight: 600;
+    grid-column: 2;
+    text-align: center;
+}
+</style>
