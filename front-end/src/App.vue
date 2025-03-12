e4642423979b24a0d1852b7241a17f58082c22cc
JonathanvdGHU <jonathan.vandegiessen@student.hu.nl>
2025-03-12 13:32:50 +0100

    home page is now responsive

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="root-element">
     <Header/>
     <main>
       <RouterView />
--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from "vue";
-import "./style.css";
+import "./app.css";
 import App from "./App.vue";
 import Router from "./router/Router.ts";
 
 // Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import 'vuetify/styles';
 
 const vuetify = createVuetify({
     components,
