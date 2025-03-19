d73d7ee6efa4e7250d02df4f4c22549889b9f0f9
wessel <wesselvandalen@gmail.com>
2025-03-19 13:24:11 +0100

    Added Google and GitHub log in, and fixed users db

--- front-end/src/pages/RegisterPage.vue
+++ front-end/src/pages/RegisterPage.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { ref } from 'vue';
-import { signUpEmailAndPassword } from '../utils/auth-service';
+import { registerWithGitHub, registerWithGoogle, signUpEmailAndPassword } from '../utils/auth-service';
 
 export default defineComponent({
     name: 'RegisterPage',
@@ -20,10 +20,31 @@ export default defineComponent({
                 })
         }
 
+        async function handleGoogleRegister(e: any) {
+            e.preventDefault();
+
+            try {
+                await registerWithGoogle();
+            } catch (error) {
+                window.alert(error);
+            }
+        };
+
+        async function handleGitHubRegister() {
+            try {
+                await registerWithGitHub();
+                window.location.reload();
+            } catch (error) {
+                window.alert(error);
+            }
+        }
+
         return {
             email,
             password,
-            handleRegister
+            handleRegister,
+            handleGoogleRegister,
+            handleGitHubRegister
         };
     }
 });
@@ -31,11 +52,23 @@ export default defineComponent({
 
 <template>
     <div class="register-page-container">
-        <form @submit="handleRegister" class="register-form">
-            <input v-model="email" type="email" placeholder="Enter email" required />
-            <input v-model="password" type="password" placeholder="Enter password" required />
-            <button type="submit" class="register-btn">Register</button>
-        </form>
+        <div class="register-page-content">
+            <form @submit="handleRegister" class="register-form">
+                <input v-model="email" type="email" placeholder="Enter email" required />
+                <input v-model="password" type="password" placeholder="Enter password" required />
+                <button type="submit" class="register-btn">Register</button>
+            </form>
+            <div class="providers-container">
+                <button @click="handleGoogleRegister">
+                    <img src="../assets/google_icon.png" alt="Google icon">
+                    Register met Google
+                </button>
+                <button @click="handleGitHubRegister">
+                    <img src="../assets/github_icon.png" alt="GitHub icon">
+                    Register met GitHub
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -49,11 +82,21 @@ export default defineComponent({
     height: 100%;
 }
 
+.register-page-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    flex-direction: column;
+    max-width: 300px;
+}
+
 .register-form {
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
+    width: 100%;
 }
 
 .register-form input {
@@ -62,7 +105,7 @@ export default defineComponent({
     border-radius: 4px;
     margin: 3px 0;
     font-size: 14px;
-
+    width: 100%;
 }
 
 .register-btn {
@@ -73,5 +116,31 @@ export default defineComponent({
     margin: 4px 0 0 0;
     color: white;
     font-size: 14px;
+}
+
+.providers-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: 5px;
+    width: 100%;
+}
+
+.providers-container button {
+    border: solid 1px rgba(0, 0, 0, .1);
+    background-color: white;
+    padding: 8px 20px;
+    width: 100%;
+    font-size: 13px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+}
+
+.providers-container button img {
+    width: 16px;
+    height: 16px;
 }
 </style>
