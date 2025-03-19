4b1aadf78e09ce3ea6dbc49de6e7984a024aab8c
JonathanvdGHU <jonathan.vandegiessen@student.hu.nl>
2025-03-19 12:40:58 +0100

    small changes after removing not used page

--- front-end/src/router/Router.ts
+++ front-end/src/router/Router.ts
@@ -2,12 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import Level from "../pages/Level.vue";
 import LevelSelect from "../pages/LevelSelect.vue";
-import Playground from "../pages/Playground.vue";
 import RegisterPage from '../pages/RegisterPage.vue';
 
 const routes = [
   { path: "/", name: "Home", component: Home },
-  { path: "/playground", name: "Playground", component: Playground },
   { path: "/level/:levelNmr", name: "Level", component: Level },
   { path: "/levelSelect", name: "levelSelect", component: LevelSelect },
   { path: "/register", name: 'RegisterPage', component: RegisterPage }
