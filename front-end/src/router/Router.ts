a89110fecce33fedbea77f2c0e663397c4d0fb22
wessel <wesselvandalen@gmail.com>
2025-03-26 14:08:44 +0100

    Built /terms route + page

--- front-end/src/router/Router.ts
+++ front-end/src/router/Router.ts
@@ -3,12 +3,14 @@ import Home from "../pages/Home.vue";
 import Level from "../pages/Level.vue";
 import LevelSelect from "../pages/LevelSelect.vue";
 import RegisterPage from '../pages/RegisterPage.vue';
+import TermsPage from "../pages/TermsPage.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/level/:levelNmr", name: "Level", component: Level },
   { path: "/levelSelect", name: "levelSelect", component: LevelSelect },
-  { path: "/register", name: 'RegisterPage', component: RegisterPage }
+  { path: "/register", name: 'RegisterPage', component: RegisterPage },
+  { path: "/terms", name: 'TermsPage', component: TermsPage }
 ];
 
 const router = createRouter({
