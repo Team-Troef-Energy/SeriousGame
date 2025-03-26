b48b2336e2a63de520ff080620b9e557d92a64bb
JonathanvdGHU <jonathan.vandegiessen@student.hu.nl>
2025-03-26 16:08:55 +0100

    added buttons and min values inside the level editor component

--- front-end/src/components/level-editor/LevelEditor.vue
+++ front-end/src/components/level-editor/LevelEditor.vue
@@ -3,15 +3,15 @@
         <form class="level-editor-form">
             <div class="form-level-input form-row">
                 <label for="levelNumber">Level Nummer</label>
-                <input type="number" id="levelNumber" v-model="levelTemplate.levelNumber" />
+                <input type="number" id="levelNumber" v-model="levelTemplate.levelNumber" min="0" />
             </div>
             <div class="form-max-co2-input form-row">
                 <label for="maxCo2">Maximale Co2</label>
-                <input type="number" id="maxCo2" v-model="levelTemplate.maxCo2" />
+                <input type="number" id="maxCo2" v-model="levelTemplate.maxCo2" min="0" />
             </div>
             <div class="form-max-coins-input form-row">
                 <label for="maxCoins">Maximaal aantal munten</label>
-                <input type="number" id="maxCoins" v-model="levelTemplate.maxCoins" />
+                <input type="number" id="maxCoins" v-model="levelTemplate.maxCoins" min="0" />
             </div>
             <div class="form-season-input form-row">
                 <label for="season">Seizoen</label>
@@ -25,27 +25,31 @@
             <div class="form-amount-of-batteries-for-transformator-input form-row">
                 <label for="amountOfBatteriesForTransformator">Aantal batterijen voor transformator</label>
                 <input type="number" id="amountOfBatteriesForTransformator"
-                    v-model="levelTemplate.amountOfBatteriesForTransformator" />
+                    v-model="levelTemplate.amountOfBatteriesForTransformator" min="0" />
             </div>
             <div class="form-costs-battery form-row">
                 <label for="costs-battery">Kosten batterij</label>
-                <input type="number" id="costs-battery" v-model="levelTemplate.resourceCosts.battery" />
+                <input type="number" id="costs-battery" v-model="levelTemplate.resourceCosts.battery" min="0" />
             </div>
             <div class="form-costs-solar-panel form-row">
                 <label for="costs-solar-panel">Kosten zonnepaneel</label>
-                <input type="number" id="costs-solar-panel" v-model="levelTemplate.resourceCosts.solarPanel" />
+                <input type="number" id="costs-solar-panel" v-model="levelTemplate.resourceCosts.solarPanel" min="0" />
             </div>
             <div class="form-costs-co2 form-row">
                 <label for="costs-co2">Kosten Co2</label>
-                <input type="number" id="costs-co2" v-model="levelTemplate.resourceCosts.co2" />
+                <input type="number" id="costs-co2" v-model="levelTemplate.resourceCosts.co2" min="0" />
             </div>
             <div class="form-start-time form-row">
                 <label for="start-time">Start tijd</label>
-                <input type="number" id="start-time" v-model="levelTemplate.startTime" />
+                <input type="number" id="start-time" v-model="levelTemplate.startTime" min="0" max="24" />
             </div>
             <div class="form-end-time form-row">
                 <label for="end-time">Eind tijd</label>
-                <input type="number" id="end-time" v-model="levelTemplate.endTime" />
+                <input type="number" id="end-time" v-model="levelTemplate.endTime" min="0" max="24" />
+            </div>
+            <div class="level-editor-buttons form-row">
+                <button class="button">Annuleren</button>
+                <button class="button">Opslaan</button>
             </div>
         </form>
     </div>
@@ -92,7 +96,6 @@ export default defineComponent({
     display: flex;
     justify-content: center;
     align-items: center;
-    overflow: auto;
     height: 100%;
 }
 
@@ -102,11 +105,13 @@ export default defineComponent({
     justify-content: center;
     align-items: center;
     text-align: center;
-    row-gap: 1rem;
+    row-gap: 0.5rem;
     background-color: white;
     border-radius: 1rem;
-    padding: 1rem;
+    padding: 2rem;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    height: 80%;
+    overflow: auto;
 }
 
 .level-editor-form label,
@@ -138,11 +143,29 @@ export default defineComponent({
     width: 80%;
 }
 
+.level-editor-buttons {
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+    width: 80%;
+    margin-top: 2rem;
+}
+
+.button {
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
+    border: none;
+    background-color: #007bff;
+    color: white;
+    cursor: pointer;
+}
+
 @media (min-width: 768px) {
     .level-editor-form {
         flex-direction: row;
         flex-wrap: wrap;
         width: 50rem;
+        height: unset;
     }
 
     .level-editor-form label {
@@ -150,10 +173,15 @@ export default defineComponent({
     }
 
     .form-row {
-        display: flex;
         justify-content: center;
         gap: 1rem;
         width: 20rem;
+    }
+
+    .level-editor-buttons {
+        justify-content: space-between;
+        gap: 1rem;
+        width: 80%;
     }
 }
 
@@ -175,9 +203,13 @@ export default defineComponent({
     }
 
     .form-row {
-        display: flex;
         gap: 1rem;
         width: 25rem;
+    }
+
+    .level-editor-buttons {
+        gap: 1rem;
+        width: 80%;
     }
 }
 </style>
