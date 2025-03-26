e2fa24352d637efc320c361597954991306978a8
JonathanvdGHU <jonathan.vandegiessen@student.hu.nl>
2025-03-26 10:49:35 +0100

    created dashboard page

--- front-end/src/router/Router.ts
+++ front-end/src/router/Router.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../pages/Dashboard.vue";
 import Home from "../pages/Home.vue";
 import Level from "../pages/Level.vue";
 import LevelSelect from "../pages/LevelSelect.vue";
@@ -8,7 +9,8 @@ const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/level/:levelNmr", name: "Level", component: Level },
   { path: "/levelSelect", name: "levelSelect", component: LevelSelect },
-  { path: "/register", name: 'RegisterPage', component: RegisterPage }
+  { path: "/register", name: 'RegisterPage', component: RegisterPage },
+  { path: "/dashboard", name: 'RegisterPage', component: Dashboard }
 ];
 
 const router = createRouter({
