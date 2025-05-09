258404eb47eadaa3a3527b8be530b051793ec2c8
JonathanvdGHU <jonathan.vandegiessen@student.hu.nl>
2025-05-09 12:51:26 +0200

    removed not used ref function

--- front-end/src/pages/race/RacePage.vue
+++ front-end/src/pages/race/RacePage.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 import { useRoute } from 'vue-router';
 import router from '../../router/Router';
 
