362325bad537d51f7256535f69903567abf4eed4
Dirk Kok <the@dirkkok.nl>
2025-05-16 21:02:34 +0200

    initial implementation of endpoints

--- back-end/src/main/java/nl/hu/serious_game/application/dto/out/RaceDTO.java
+++ back-end/src/main/java/nl/hu/serious_game/application/dto/out/RaceDTO.java
@@ -1,9 +1,19 @@
 package nl.hu.serious_game.application.dto.out;
 
+import nl.hu.serious_game.domain.Race;
+
 import java.util.List;
 
 public record RaceDTO(
         String name,
         String userEmail,
         List<LevelTemplateDTO> levels
-) {}
+) {
+    public static RaceDTO fromEntity(Race race) {
+        return new RaceDTO(
+                race.getName(),
+                race.getUserEmail(),
+                race.getLevels().stream().map(LevelTemplateDTO::fromEntity).toList()
+        );
+    }
+}
--- back-end/src/main/java/nl/hu/serious_game/data/RaceRepository.java
+++ back-end/src/main/java/nl/hu/serious_game/data/RaceRepository.java
@@ -3,5 +3,8 @@ package nl.hu.serious_game.data;
 import nl.hu.serious_game.domain.Race;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RaceRepository extends JpaRepository<Race, Long> {
+    Optional<Race> findByUserEmail(String userEmail);
 }
