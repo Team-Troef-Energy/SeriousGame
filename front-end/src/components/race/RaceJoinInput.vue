7ba7d6e51fcc2395cf422148c6bbefd7daec1667
JonathanvdGHU <jonathan.vandegiessen@student.hu.nl>
2025-05-28 11:54:28 +0200

    created racejoin input component

---
+++ front-end/src/components/race/RaceJoinInput.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="race-join-input">
+        <form @submit="handleSubmit">
+            <input type="text" v-model="input" :placeholder="placeholder" />
+            <button type="submit"> {{ buttonText }}</button>
+        </form>
+        <Teleport to="body">
+            <TextModal :show="isTextModalVisible" :content="textModalContent" @close="isTextModalVisible = false" />
+        </Teleport>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue';
+import TextModal from '../global/modals/TextModal.vue';
+import { textModal } from '../../types/global/modals/TextModal';
+
+export default defineComponent({
+    name: 'RaceJoinInput',
+    components: { TextModal },
+    props: {
+        placeholder: {
+            type: String,
+            required: true,
+        },
+        buttonText: {
+            type: String,
+            required: true,
+        },
+
+        errorMessage: {
+            type: String,
+            required: false,
+        },
+    },
+    setup(props, { emit }) {
+        let isTextModalVisible = ref(false)
+
+        let textModalContent = ref<textModal>({
+            header: 'Alert',
+            body: 'Nothing to show'
+        });
+
+        const showModal = (header: string, body: string) => {
+            textModalContent.value.header = header;
+            textModalContent.value.body = body;
+            isTextModalVisible.value = true;
+        };
+
+        const input = ref('');
+
+        const handleSubmit = (event: Event) => {
+            event.preventDefault();
+            const trimmedInput = input.value.trim();
+            emit('givenInput', trimmedInput);
+        };
+
+        watch(
+            () => props.errorMessage,
+            (newVal) => {
+                if (newVal) {
+                    if (newVal !== '') {
+                        showModal('Foutmelding', newVal);
+                    }
+                }
+            }
+        );
+
+        return {
+            input,
+            handleSubmit,
+            isTextModalVisible,
+            textModalContent,
+        };
+    }
+});
+</script>
+
+<style scoped>
+.race-join-input {
+    display: flex;
+    align-items: center;
+    width: 80%;
+    height: 12rem;
+    border: .5px solid transparent;
+    border-radius: 1rem;
+    background-image: linear-gradient(white, white), linear-gradient(to right, var(--troef-green), var(--troef-blue));
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    gap: 1rem;
+}
+
+input {
+    text-align: center;
+    width: 80%;
+    height: 3rem;
+    color: rgb(51, 51, 51);
+    border: .5px solid transparent;
+    background-image: linear-gradient(white, white), linear-gradient(to right, var(--troef-green), var(--troef-blue));
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+}
+
+button {
+    width: 80%;
+    height: 3rem;
+    color: white;
+    font-weight: bold;
+    border-radius: 0.5rem;
+    border: none;
+    cursor: pointer;
+    background-image: linear-gradient(to right, var(--troef-green), var(--troef-blue));
+}
+
+@media (min-width: 475px) {
+    .race-join-input {
+        width: 20rem;
+    }
+}
+</style>
