9010cfe6ee9537bbde2074657b5ce06267895650
Dirk Kok <the@dirkkok.nl>
2025-05-30 16:18:38 +0200

    include RaceSessionUsers in getRaceSession* endpoints

--- back-end/src/main/java/nl/hu/serious_game/application/dto/out/RaceSessionDTO.java
+++ back-end/src/main/java/nl/hu/serious_game/application/dto/out/RaceSessionDTO.java
@@ -2,17 +2,28 @@ package nl.hu.serious_game.application.dto.out;
 
 import nl.hu.serious_game.domain.Race;
 import nl.hu.serious_game.domain.RaceSession;
+import nl.hu.serious_game.domain.RaceSessionUser;
+
+import java.util.List;
 
 public record RaceSessionDTO(
         Long id,
         Race race,
-        String joinCode
+        String joinCode,
+        List<RaceSessionUserDTO> users
 ) {
     public static RaceSessionDTO fromEntity(RaceSession session) {
+        List<RaceSessionUserDTO> users = null;
+
+        if (session.getUsers() != null) {
+            users = session.getUsers().stream().map(RaceSessionUserDTO::fromEntity).toList();
+        }
+
         return new RaceSessionDTO(
                 session.getId(),
                 session.getRace(),
-                session.getJoinCode()
+                session.getJoinCode(),
+                users
         );
     }
 }
--- back-end/src/main/java/nl/hu/serious_game/domain/RaceSession.java
+++ back-end/src/main/java/nl/hu/serious_game/domain/RaceSession.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nl.hu.serious_game.application.dto.out.RaceSessionUserDTO;
+
+import java.util.List;
 
 @Getter
 @Entity
@@ -19,6 +22,9 @@ public class RaceSession {
 
     @Column
     private String joinCode;
+
+    @OneToMany(mappedBy = "raceSession")
+    private List<RaceSessionUser> users;
 
     public RaceSession(Race race, String joinCode) {
         this.race = race;
