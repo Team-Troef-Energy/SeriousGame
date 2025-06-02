5dde6e94d638c0600622c41c304dda1ef4644c25
Dirk Kok <the@dirkkok.nl>
2025-06-02 09:38:16 +0200

    fix using entity in DTO; recursion bug

--- back-end/src/main/java/nl/hu/serious_game/application/dto/out/RaceSessionDTO.java
+++ back-end/src/main/java/nl/hu/serious_game/application/dto/out/RaceSessionDTO.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public record RaceSessionDTO(
         Long id,
-        Race race,
+        RaceDTO race,
         String joinCode,
         List<RaceSessionUserDTO> users
 ) {
@@ -21,7 +21,7 @@ public record RaceSessionDTO(
 
         return new RaceSessionDTO(
                 session.getId(),
-                session.getRace(),
+                RaceDTO.fromEntity(session.getRace()),
                 session.getJoinCode(),
                 users
         );
