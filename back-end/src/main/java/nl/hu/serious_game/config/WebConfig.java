deec3be7aceba5e6751e56be24a59dd9ad8bc988
Dirk Kok <the@dirkkok.nl>
2025-06-19 23:16:44 +0200

    move backend allowed cors origins to config property

--- back-end/src/main/java/nl/hu/serious_game/config/WebConfig.java
+++ back-end/src/main/java/nl/hu/serious_game/config/WebConfig.java
@@ -1,22 +1,21 @@
 package nl.hu.serious_game.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.allowed-origins}")
+    private String allowedCorsOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println(allowedCorsOrigins);
+
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173",
-                        "https://troefgame.duckdns.org",
-                        "https://dev.troefgame.jonaqhan.nl",
-                        "https://frontend.dev.troefgame.jonaqhan.nl",
-                        "https://troefgamedev.duckdns.org"
-                )
+                .allowedOrigins(allowedCorsOrigins.split(";"))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
